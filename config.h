7c0c003cf21bd34c8e22ff6e43140201cb830720
Adrian Staniec <adrianstaniec@gmail.com>
2020-10-16 09:26:22 +0200

    Revert "add swallow patch"
    This reverts commit 1fb4ed0e0a91be8b87ac0e131ab25d489715ea2c.
    Doesn't work perfectly:
    - doesn't swallow when running "$ code" from terminal
    - once it resulted in a weird odd-sized floating window
    needs further investigation, but not crucial to me at the moment

--- config.h
+++ config.h
@@ -15,7 +15,6 @@ static const unsigned int systrayspacing = 2;   /* systray spacing */
 static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
 static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
-static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "UbuntuMono Nerd Font Mono:pixelsize=18:antialias=true:autohint=true" };
 static const char dmenufont[]       = "Liberation Mono:pixelsize=17:antialias=true:autohint=true";
@@ -39,11 +38,10 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class       instance  title           tags mask  isfloating  isterminal  noswallow  monitor */
-	{ "Audacious", NULL,     NULL,           1 << 8,    0,          1,           0,        -1 },
-	{ "Gimp",      NULL,     NULL,           0,         1,          0,           0,        -1 },
-	{ "St",        NULL,     NULL,           0,         0,          1,           0,        -1 },
-	{ NULL,        NULL,     "Event Tester", 0,         0,          0,           1,        -1 }, /* xev */
+	/* class       instance  title           tags mask  isfloating  monitor */
+	{ "Audacious", NULL,     NULL,           1 << 8,    0,          -1 },
+	{ "Gimp",      NULL,     NULL,           0,         1,          -1 },
+	{ "St",        NULL,     NULL,           0,         0,          -1 },
 };
 
 /* layout(s) */
